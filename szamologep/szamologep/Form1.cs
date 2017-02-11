6ddcb9944ba8450147423b8603dd00790add0588
Bman <Bman@BMAN-PC>
2017-02-11 18:00:30 +0100

    Gombok kész

--- szamologep/szamologep/Form1.cs
+++ szamologep/szamologep/Form1.cs
@@ -16,5 +16,30 @@ namespace szamologep
         {
             InitializeComponent();
         }
+
+        private void gomb_nyomas(object sender, EventArgs e)//az összes szám-gomb lenyomására vonatkozik
+        {
+            Button Gomb = (Button)sender;
+            textBox1.Text = textBox1.Text + Gomb.Text;
+        }
+
+        private void elojel_valtoztatas(object sender, EventArgs e)
+        {
+            if (textBox1.Text.StartsWith("-"))
+                textBox1.Text = textBox1.Text.Substring(1);
+            else if (!string.IsNullOrEmpty(textBox1.Text) && decimal.Parse(textBox1.Text) != 0)
+                textBox1.Text = "-" + textBox1.Text;
+        }
+
+        private void C_gomb(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+        }
+
+        private void CE_gomb(object sender, EventArgs e)
+        {
+            textBox1.Text = "0";
+           
+        }
     }
 }
